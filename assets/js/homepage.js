2a055ad91cc069ede8748718191ceb8ce04ccf38
echo <miyunniecho@gmail.com>
2021-01-18 21:28:35 -0800

    submit and fetch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,14 +1,96 @@
+//Global 
+let userFormEl = document.querySelector("#user-form");
+let nameInputEl = document.querySelector("#username");
+let repoContainerEl = document.querySelector("#repos-container");
+let repoSearchTerm = document.querySelector("#repo-search-term");
+
 //Make an HTTP request to the GitHub API using fetch()
-let getUserRepos = function(user) {
+let getUserRepos = function (user) {
     // format the github api url
     let apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
+
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
+    fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data, user);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+      alert("Unable to connect to GitHub");
     });
-  };
-  
-  getUserRepos()
+};
+
+//event.preventDefault() stops the broswer from preforming the default action the even wants it is do; 
+//i.e. in submitting a fomr, it prevents the browser from sending the form's input data to a URL
+let formSubmitHandler = function (event) {
+    event.preventDefault();
+    console.log(event);
+};
+
+// get value from input element
+let username = nameInputEl.value.trim();
+
+if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+} else {
+    alert("Please enter a GitHub username");
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+let displayRepos = function (repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+
+    console.log(repos);
+    console.log(searchTerm);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
+};
