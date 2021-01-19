ca19f82ce94fea5ee4a76c520cb2608db201fa26
echo <miyunniecho@gmail.com>
2021-01-18 21:05:00 -0800

    fetch

---
+++ assets/js/homepage.js
@@ -0,0 +1,14 @@
+//Make an HTTP request to the GitHub API using fetch()
+let getUserRepos = function(user) {
+    // format the github api url
+    let apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
+  
+  getUserRepos()
